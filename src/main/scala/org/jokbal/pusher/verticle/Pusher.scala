9614c35151426cd895b18d9467bc2a08406edc37
tokirin11117 <tokirin1117@gmail.com>
2013-11-08 04:57:17 +0900

    restore wrong merge

---
+++ src/main/scala/org/jokbal/pusher/verticle/Pusher.scala
@@ -0,0 +1,28 @@
+package org.jokbal.puhser.verticle
+
+import org.vertx.scala.platform.Verticle
+import org.vertx.scala.core.eventbus.EventBus
+
+object Pusher{
+  var apikey = ""
+  var secret = ""
+  var eventbus_prefix = ""
+  var shareddata_prefix = ""
+
+}
+
+class Pusher extends Verticle {
+  var eventBus: EventBus = null
+
+  override def start() {
+    val config = container.config()
+    initialization
+  }
+
+  def initialization() {
+    eventBus = vertx.eventBus
+    println("test")
+    container.deployVerticle("scala:org.jokbal.puhser.verticle.SocketServer")
+    println("test2")
+  }
+}
